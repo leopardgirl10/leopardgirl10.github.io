bd7723c277c8058f3e9d2cb7d1ca269a35a7c82c
leopardgirl1010 <echaney1010@gmail.com>
2016-08-10 11:15:23 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -6,6 +6,12 @@ class BootState {
   }
 }
 
+class LoadState {
+  create()
+  console.log("Loading")
+}
+
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
 game.state.start("Boot");
+game.state.add("LoadState")
