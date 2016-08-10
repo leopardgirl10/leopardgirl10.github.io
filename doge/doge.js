b818e88777cf67f2fccc98ac3de76fd4d3ade114
leopardgirl1010 <echaney1010@gmail.com>
2016-08-10 11:09:23 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -1,1 +1,11 @@
-var game = new Phaser.Game(320.568);
+class BootState {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVirtically = true;
+  }
+}
+
+var game = new Phaser.Game(320,568);
+game.state.add("Boot",BootState);
+game.state.start("Boot");
