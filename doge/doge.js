79557aa92d092589bd4578ebadb1fd7d2cb15f00
leopardgirl1010 <echaney1010@gmail.com>
2016-08-10 11:45:06 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -15,12 +15,22 @@ class BootState {
 class LoadState {
   preload() {
     console.log("Loading...");
+    this.load.image("background", "assets/background.png")
   }
   create() {
     console.log("Loaded");
+    game.state.start("Play")
   }
 }
-  
+
+class PlayStart {
+  create(){
+    console.log("Entered Play Start");
+    this.background = this.add.tileSprite(0,o,320,568,"background");
+    this.background.autoScroll(0,700);
+  }
+}
+
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
 game.state.add("Load",LoadState);
