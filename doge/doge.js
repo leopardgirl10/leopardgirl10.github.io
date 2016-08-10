6a897fee47b0496f27e76546494d6f77686ca01b
leopardgirl1010 <echaney1010@gmail.com>
2016-08-10 11:21:44 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -4,14 +4,25 @@ class BootState {
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVirtically = true;
   }
+
+
+  create() {
+  game.state.start("Load")
+  }
 }
 
-class LoadState {
-  create()
-  console.log("Loading")
-}
 
+class LoadTsate {
+  preload() {
+    console.log("Loading...");
+  }
+  create() {
+    console.log("Loaded");
+  }
+}
+  
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
+game.state.add("Load",LoadState);
 game.state.start("Boot");
-game.state.add("LoadState")
+
